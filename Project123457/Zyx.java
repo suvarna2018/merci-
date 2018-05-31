bd587af2852f3c9baf6bdace98ff77a8e003b736
suvarna2018 <suvarnashankarp@gmail.com>
2018-05-31 11:18:33 +0530

    hi

--- Project123457/Zyx.java
+++ Project123457/Zyx.java
@@ -1,11 +1,11 @@
 
-public class xyz   {
+public class xyz     {
 
-	public static void main(String[] args) {
+	public static  void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("welcome");
 		System.out.println("hi");
-		System.out.println("hellow");
+		System.out.println("hello");
 	}
 	
 
